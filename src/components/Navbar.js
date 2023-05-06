85dcd274a74d7d6194cb72c08411aa059938bdf2
amassock <ajmassock@gmail.com>
2023-05-06 16:49:52 -0500

    add tailwind and Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Bills</a>
+      <a href="#">Congress Members</a>
+      <a href="#">Senate Members</a>
+      <a href="#">Bills</a>
+    </nav>
+  );
+}
