cf11043f5fab45a8ff4cf9f30573c3e6797646d9
amassock <ajmassock@gmail.com>
2023-05-16 21:17:29 -0500

    map through api response

--- src/components/Bills.js
+++ src/components/Bills.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 let url =
   "https://api.congress.gov/v3/bill/117/hr?api_key=a9OP2jf04tL6ExMYMzucnqqxJA3cpV8EMhBrYA9H";
@@ -12,10 +13,62 @@ export default function Bills() {
         .then((response) => response.json())
         .then((data) => {
           setBillData(data);
+        })
+        .catch((err) => {
+          console.log(err);
         });
     };
     fetchData();
   }, []);
 
-  return console.log(billData);
+  if (!billData.bills) {
+    return <div>Loading...</div>;
+  }
+
+  console.log(billData.bills[0]);
+  console.log(typeof billData.bills);
+
+  let data = billData.bills.map(
+    ({
+      congress,
+      latestAction,
+      actionDate,
+      text,
+      number,
+      originChamber,
+      originChamberCode,
+      title,
+      type,
+      updateDate,
+      updateDateIncludingText,
+      url,
+    }) => {
+      return (
+        <div>
+          <Link
+            to={`/bill/${number}`}
+            location={{
+              congress: congress,
+              latestAction: latestAction,
+              actionDate: actionDate,
+              text: text,
+              number: number,
+              originChamber: originChamber,
+              originChamberCode: originChamberCode,
+              title: title,
+              type: type,
+              updateDate: updateDate,
+              updateDateIncludingText: updateDateIncludingText,
+              url: url,
+            }}
+          >
+            <div>
+              <div>{title}</div>
+            </div>
+          </Link>
+        </div>
+      );
+    }
+  );
+  return <div>{data}</div>;
 }
