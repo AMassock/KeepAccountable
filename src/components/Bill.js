dca6d59f8641720b4459c4577e7b98397264c2c9
amassock <ajmassock@gmail.com>
2023-05-22 19:40:24 -0500

    add styling and update bill component

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -3,7 +3,8 @@ import { useParams } from "react-router-dom";
 
 const BillDetails = () => {
   const { congress, type, number } = useParams();
-  const [bill, setBill] = useState(null);
+  const [bill, setBill] = useState();
+  const [details, setDetails] = useState();
 
   useEffect(() => {
     const fetchBill = async () => {
@@ -21,18 +22,35 @@ const BillDetails = () => {
     fetchBill();
   }, []);
 
-  console.log(bill.bill);
+  useEffect(() => {
+    const fetchDetails = async () => {
+      try {
+        const response = await fetch(
+          `https://api.congress.gov/v3/bill/${congress}/${type}/${number}/summaries?api_key=a9OP2jf04tL6ExMYMzucnqqxJA3cpV8EMhBrYA9H`
+        );
+        const data = await response.json();
+        setDetails(data);
+      } catch (error) {
+        console.error("Error fetching bill details:", error);
+      }
+    };
+
+    fetchDetails();
+  }, []);
 
   if (!bill) {
     return <div>Loading...</div>;
   }
+  let sponsor = bill.bill.sponsors;
 
   return (
     <div>
       <h1>Bill Details</h1>
-      <h2>{bill.bill.title}</h2>
-      <p>{bill.bill.description}</p>
-      <p>Sponsor: {bill.bill.sponsor}</p>
+      <h2>Title: {bill.bill.title}</h2>
+      <p>Description: {bill.bill.description}</p>
+      {sponsor.map((obj, index) => (
+        <p key={index}>{JSON.stringify(obj.fullName)}</p>
+      ))}
       {/* Add more bill details as needed */}
     </div>
   );
