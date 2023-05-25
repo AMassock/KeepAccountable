4fee2f62576a8d7cfee7b493dcd5a8f81ec98789
amassock <ajmassock@gmail.com>
2023-05-25 12:08:12 -0500

    fix authform requests

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -3,38 +3,80 @@ import React, { useState } from "react";
 const AuthForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
   const [isLogin, setIsLogin] = useState(true);
 
-  const handleSubmit = async (e) => {
+  //   const handleSubmit = async (e) => {
+  //     e.preventDefault();
+
+  //     const apiUrl = "localhost:8000";
+  //     const endpoint = isLogin ? "login/" : "register/";
+
+  //     try {
+  //       const response = await fetch(apiUrl + endpoint, {
+  //         method: "POST",
+  //         mode: "cors",
+  //         username,
+  //         password,
+  //       });
+
+  //       // Handle the response
+  //       console.log(response.data);
+  //     } catch (error) {
+  //       // Handle the error here
+  //       console.error(error);
+  //     }
+  //   };
+
+  const handleRegister = async (e) => {
     e.preventDefault();
-
-    const apiUrl = "localhost:8000";
-    const endpoint = isLogin ? "login/" : "create/";
-
     try {
-      const response = await fetch(apiUrl + endpoint, {
+      const response = await fetch("http://localhost:8000/register/", {
         method: "POST",
         mode: "cors",
-        username,
-        password,
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
       });
 
-      // Handle the response according to your needs
-      console.log(response.data);
+      const data = await response.json();
+      setMessage(data.success || data.error);
     } catch (error) {
-      // Handle the error here
-      console.error(error);
+      setMessage("An error occurred. Please try again.");
+    }
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:8000/login/", {
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+      setMessage(data.success || data.error);
+    } catch (error) {
+      setMessage("An error occurred. Please try again.");
     }
   };
 
   return (
     <div className="auth-container">
       <h2>{isLogin ? "Login" : "Create Account"}</h2>
-      <form onSubmit={handleSubmit}>
+      <form>
         <div>
           <label>Username:</label>
           <input
             type="text"
+            placeholder="Username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
@@ -43,15 +85,19 @@ const AuthForm = () => {
           <label>Password:</label>
           <input
             type="password"
+            placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <button type="submit">{isLogin ? "Login" : "Create Account"}</button>
+        {/* <button type="submit">{isLogin ? "Login" : "Create Account"}</button> */}
       </form>
-      <p onClick={() => setIsLogin(!isLogin)}>
+      {/* <p onClick={() => setIsLogin(!isLogin)}>
         {isLogin ? "Create an account" : "Already have an account? Login"}
-      </p>
+      </p> */}
+      <button onClick={handleRegister}>Register</button>
+      <button onClick={handleLogin}>Login</button>
+      <p>{message}</p>
     </div>
   );
 };
